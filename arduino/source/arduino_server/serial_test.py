e32d5a7b80a6a528ae7e2aa39eebd2db4ea41779
Iliya Iliev <iliyailiev3592@gmail.com>
2019-10-20 21:17:26 +0300

    Use python2 instead python3 for the serial test script
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- arduino/source/arduino_server/serial_test.py
+++ arduino/source/arduino_server/serial_test.py
@@ -1,24 +1,41 @@
+
 import serial
 import time
 
 port = "/dev/ttyACM0"
+ser = ""
 
         # VAlid
-cmds = ["S>01<E*", "S>0401DATA<E*", \
+cmds0 = ["S>01<E*", "S>0401DATA<E*", \
         # Invalid
         "0101DAdsdddddddddddddddddTAdssdasdas*",
         "S>0101DAdsdddddddddddddddddTAdssdasdas<E*",
-        "S>0101DAdsdddddddddddddddddTAdssdasdas*"]
+        "S>0101DAdsdddddddddddddddddTAdssdasdas*",
+        "S>05<E*",]
 
-try:
-    ser = serial.Serial(port, 9600, timeout=5)
-    ser.close()
-    ser.open()
+cmds1 = ["S>01<E*","S>0401DATA1<E*", "S>0401DATA2<E*", \
+         "S>0401DATA3<E*", "S>05<E*"]
+
+def run_cmds(cmds):
+    global ser
 
     for data in cmds:
-        ser.write(data.encode())
-        time.sleep(2)
-        read_val = ser.readline()
-        print(read_val)
-except serial.SerialException:
-    pass
+        ser.write(data)
+        time.sleep(0.01)
+
+def main():
+    global ser
+
+    try:
+        ser = serial.Serial(port, 38400, timeout=5)
+        ser.close()
+        ser.open()
+    except serial.SerialException:
+        print("Failed to open the serial port!")
+        pass
+    else:
+        run_cmds(cmds1)
+
+
+if __name__ == "__main__":
+    main()
